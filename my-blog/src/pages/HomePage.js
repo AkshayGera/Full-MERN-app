0aeb3bacc2eb5401c2ccd7390e6a88d23a9e9658
AkshayGera <akshay16gera@gmail.com>
2022-02-04 13:59:39 -0600

    initial commit

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <><h1>Hello, welcome to my blog</h1><p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatum, quisquam.
+    </p></>
+);
+
+export default HomePage;
